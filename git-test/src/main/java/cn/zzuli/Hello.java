19a6fda59002dc840f2742c2fcd779b786015b21
simpleJun <3061406488@qq.com>
2022-05-08 10:48:44 +0800

    third commit hello

--- git-test/src/main/java/cn/zzuli/Hello.java
+++ git-test/src/main/java/cn/zzuli/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello git");
+	System.out.println("我很帅不用你说我知道");
     }
 }
